354c73a62d725be6f5025bced4c860f894bd62ca
TerryLew <terrysatou@gmail.com>
2018-10-10 22:03:03 -0700

    set state prototype

--- LOTlib/testWorldState.py
+++ LOTlib/testWorldState.py
@@ -22,8 +22,11 @@ WS_0.moveBall('bucket_3', 'bucket_0', 'black')
 print '3: ' + str(WS_0==WS_1)
 print str(WS_0) + '\n-----------------------\n' + str(WS_1)
 
+state = {
+	'bucket_0': Bucket(n_black_ball=1, n_red_ball=2, n_green_ball=3)
+	'bucket_1': Bucket(n_black_ball=4, n_red_ball=5, n_green_ball=6)
+	'bucket_2': Bucket(n_black_ball=7, n_red_ball=8, n_green_ball=9)
+	'bucket_3': Bucket(n_black_ball=10, n_red_ball=11, n_green_ball=12)
+}
 
-
-
-
-
+WS_0.setState(state)
