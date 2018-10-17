5f26b943cfc23f2655935dae1f364a59dc80bc14
TerryLew <terrysatou@gmail.com>
2018-10-17 01:16:43 -0700

    add container base class and hand class

--- LOTlib/WorldState.py
+++ LOTlib/WorldState.py
@@ -1,72 +1,154 @@
 from LOTlib.Miscellaneous import self_update
 from LOTlib.DataAndObjects import Obj
+from sets import Set
+import sys
 
-class Bucket():
+possible_colors = Set([
+	'black',
+	'red',
+	'green'
+	])
+
+class Container(object):
 	def __init__(self, **kwargs):
-		self._balls = {'black': 0,
-				 'red': 0,
-				 'green': 0}
-		# Since we apply to this, it must be a list
-		if len(kwargs) != 0:
-			assert len(kwargs)==3, "Need to specify number of black balls, red balls, and green balls."
-			self._balls['black'] = kwargs.get('black')
-			self._balls['red'] = kwargs.get('red')
-			self._balls['green'] = kwargs.get('green')
+		self._balls = {}
+		for color in possible_colors:
+			self._balls[color] = 0
+		for color, number in kwargs.items():
+			if not color in possible_colors:
+				sys.exit('[Error]: Color \'' + color + '\' is not allowed.' )
+			elif not isinstance(number, int):
+				sys.exit('[Error]: \'' + number + '\' is not an integer.' )
+			else:
+				self._balls[color] = number
+		self._capacity = 1000
 
 	def __eq__(self, other):
-		return self._balls['black'] == other._balls['black'] and self._balls['red'] == other._balls['red'] and self._balls['green'] == other._balls['green']
-
- 	def __str__(self):
-		outstr = '<Black: ' + str(self._balls['black']) + ', '
-		outstr = outstr + 'Red: ' + str(self._balls['red']) + ', '
-		outstr = outstr + 'Green: ' + str(self._balls['green']) + '>'
-		return outstr
-
-	def __repr__(self): # used for being printed in lists
-		return str(self)
-
-class WorldState():
-	def __init__(self, **kwargs):
-		self._buckets = {}
-
-		assert 'n_black_ball' in kwargs and 'n_red_ball' in kwargs and 'n_green_ball' in kwargs, "Need n_black_ball, n_red_ball, and n_red_ball"
-		self._buckets['bucket_0'] = Bucket(black=kwargs.get('n_black_ball'), red=0, green=0)
-		self._buckets['bucket_1'] = Bucket(black=0, red=kwargs.get('n_red_ball'), green=kwargs.get('n_green_ball'))
-
-		self._buckets['bucket_2'] = Bucket()
-		self._buckets['bucket_3'] = Bucket()
-
-	def __eq__(self, other):
-		source_bucket_0_is_equal = (self._buckets['bucket_0'] == other._buckets['bucket_0'])
-		source_bucket_1_is_equal = (self._buckets['bucket_1'] == other._buckets['bucket_1'])
-		target_bucket_0_is_equal = (self._buckets['bucket_2'] == other._buckets['bucket_2'])
-		target_bucket_1_is_equal = (self._buckets['bucket_3'] == other._buckets['bucket_3'])
-		return source_bucket_0_is_equal and source_bucket_1_is_equal and target_bucket_0_is_equal and target_bucket_1_is_equal
+		for color, number in self._balls.items():
+			if color not in other._balls.keys():
+				return False
+			elif other._balls[color] != number:
+				return False
+		for color, number in other._balls.items():
+			if color not in self._balls.keys():
+				return False
+			elif self._balls[color] != number:
+				return False
+		return True
 
 	def __str__(self):
-		outstr = '<WorldState: \n'
-		outstr = outstr + 'Bucket 0: ' + str(self._buckets['bucket_0']) + '\n'
-		outstr = outstr + 'Bucket 1: ' + str(self._buckets['bucket_1']) + '\n'
-		outstr = outstr + 'Bucket 2: ' + str(self._buckets['bucket_2']) + '\n'
-		outstr = outstr + 'Bucket 3: ' + str(self._buckets['bucket_3']) + '\n'
+		outstr = '<'
+		for color, number in self._balls.items():
+			outstr = outstr + color + ': ' + str(number) + ' '
 		outstr = outstr + '>'
 		return outstr
 
 	def __repr__(self): # used for being printed in lists
 		return str(self)
 
-	def existColor(self, bucket, color):
-		if color=='black':
-			return self._buckets[bucket]._balls['black']>0
-		elif color=='red':
-			return self._buckets[bucket]._balls['red']>0
-		elif color=='green':
-			return self._buckets[bucket]._balls['green']>0
+	def remove_all_balls(self):
+		for color, number in self._balls.items():
+			number = 0
+
+class Bucket(Container):
+	def __init__(self, *args, **kwargs):
+		super(Bucket, self).__init__(**kwargs)
+		for arg in args:
+			if isinstance(arg, Bucket):
+				for color, number in arg._balls.items():
+					self._balls[color] = number
+			else:
+				sys.exit('[Error]: expect a Bucket object.')
+		self._capacity = 1000
+
+	def get_vacancy(self):
+		vacancy = self._capacity
+		for color, number in self._balls.items():
+			vacancy = vacancy - number
+		return vacancy
+
+
+class Hand(Container):
+	def __init__(self, *args, **kwargs):
+		super(Hand, self).__init__(**kwargs)
+		for arg in args:
+			if isinstance(arg, Hand):
+				for color, number in arg._balls.items():
+					self._balls[color] = number
+			else:
+				sys.exit('[Error]: expect a Hand object.')
+		self._capacity = 3
+
+	def get_vacancy(self):
+		vacancy = self._capacity
+		for color, number in self._balls.items():
+			vacancy = vacancy - number
+		return vacancy
+
+
+class WorldState():
+	def __init__(self, *args):
+		self._container = {
+			'bucket_0': Bucket(),
+			'bucket_1': Bucket(),
+			'bucket_2': Bucket(),
+			'bucket_3': Bucket(),
+			'hand_left': Hand(),
+			'hand_right': Hand()
+
+		}
+		self.setWorldState(*args)
+
+	def __eq__(self, other):
+		bucket_0_is_equal = (self._container['bucket_0'] == other._container['bucket_0'])
+		bucket_1_is_equal = (self._container['bucket_1'] == other._container['bucket_1'])
+		bucket_2_is_equal = (self._container['bucket_2'] == other._container['bucket_2'])
+		bucket_3_is_equal = (self._container['bucket_3'] == other._container['bucket_3'])
+		hand_left_is_equal = (self._container['hand_left'] == other._container['hand_left'])
+		hand_right_is_equal = (self._container['hand_right'] == other._container['hand_right'])
+		return bucket_0_is_equal and bucket_1_is_equal and bucket_0_is_equal and bucket_1_is_equal and hand_left_is_equal and hand_right_is_equal
+
+	def __str__(self):
+		outstr = '<WorldState: \n'
+		outstr = outstr + 'bucket_0: ' + str(self._container['bucket_0']) + '\n'
+		outstr = outstr + 'bucket_1: ' + str(self._container['bucket_1']) + '\n'
+		outstr = outstr + 'bucket_2: ' + str(self._container['bucket_2']) + '\n'
+		outstr = outstr + 'bucket_3: ' + str(self._container['bucket_3']) + '\n'
+		outstr = outstr + 'hand_left: ' + str(self._container['hand_left']) + '\n'
+		outstr = outstr + 'hand_right: ' + str(self._container['hand_right']) + '\n'
+		outstr = outstr + '>'
+		return outstr
+
+	def __repr__(self): # used for being printed in lists
+		return str(self)
+
+	def setWorldState(self, state):
+		for container_name, container in self._container.items():
+			container.remove_all_balls()
+		for container_name, container in state.items():
+			if isinstance(container, Bucket):
+				self._container[container_name] = Bucket(container)
+			elif isinstance(container, Hand):
+				self._container[container_name] = Hand(container)
+			else:
+				sys.exit('[Error]: expect Bucket object or Hand object.')
+
+	def existColor(self, container, color, number=1):
+		if container not in self._container.keys():
+			sys.exit('[Error]: number of ' + color + ' balls in ' + container + ' is less than' + number)
+		elif color in self._container[container]._balls:
+			return self._container[container]._balls[color] >= number
 		else:
 			return False
 
-	def moveBall(self, from_bucket, to_bucket, color):
-		assert from_bucket in self._buckets and to_bucket in self._buckets, "Incorrect bucket name"
-		if self.existColor(from_bucket, color):
-			self._buckets[from_bucket]._balls[color] = self._buckets[from_bucket]._balls[color] - 1
-			self._buckets[to_bucket]._balls[color] = self._buckets[to_bucket]._balls[color] + 1
+	def canAddBall(self, container, number=1):
+		return self._container[container].get_vacancy() >= number
+
+	def moveBall(self, from_container, to_container, color, number=1):
+		assert from_container in self._container, "Incorrect source container name: "+from_container
+		assert to_container in self._container, "Incorrect target container name: "+to_container
+
+		if self.existColor(from_container, color, number) and self.canAddBall(to_container, number):
+			self._container[from_container]._balls[color] = self._container[from_container]._balls[color] - number
+			self._container[to_container]._balls[color] = self._container[to_container]._balls[color] + number
+
--- LOTlib/testWorldState.py
+++ LOTlib/testWorldState.py
@@ -1,33 +1,84 @@
 from LOTlib.DataAndObjects import Obj
 from LOTlib.WorldState import *
 
-WS_0 = WorldState(n_black_ball=1, n_red_ball=4, n_green_ball=4)
-WS_1 = WorldState(n_black_ball=1, n_red_ball=4, n_green_ball=4)
-
-print '0: ' + str(WS_0==WS_1)
-print str(WS_0) + '\n-----------------------\n' + str(WS_1)
-print '========================================='
-
-WS_0.moveBall('bucket_0', 'bucket_3', 'black')
-print '1: ' + str(WS_0==WS_1)
-print str(WS_0) + '\n-----------------------\n' + str(WS_1)
-print '========================================='
-
-WS_0.moveBall('bucket_0', 'bucket_3', 'black')
-print '2: ' + str(WS_0==WS_1)
-print str(WS_0) + '\n-----------------------\n' + str(WS_1)
-print '========================================='
-
-WS_0.moveBall('bucket_3', 'bucket_0', 'black')
-print '3: ' + str(WS_0==WS_1)
-print str(WS_0) + '\n-----------------------\n' + str(WS_1)
-
-state = {
-	'bucket_0': Bucket(black=1, red=2, green=3)
-	'bucket_1': Bucket(black=4, red=5, green=6)
-	'bucket_2': Bucket(black=7, red=8, green=9)
-	'bucket_3': Bucket(black=10, red=11, green=12)
+init_state = {
+	'bucket_0': Bucket(black=1, red=0, green=0),
+	'bucket_1': Bucket(black=0, red=4, green=4),
+	'bucket_2': Bucket(black=0, red=0, green=0),
+	'bucket_3': Bucket(black=0, red=0, green=0),
+	'hand_left': Hand(black=1),
+	'hand_right': Hand()
 }
 
-WS_0.setState(state)
-print str(WS_0)
+def testWorldStateEqual():
+	WS_0 = WorldState(init_state)
+	WS_1 = WorldState(init_state)
+
+	print '0: ' + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n' + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_0', 'bucket_3', 'black')
+	print '1: ' + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n' + str(WS_1)
+	print '========================================='
+
+def testMoveBalls():
+	WS_0 = WorldState(init_state)
+	WS_0.moveBall('bucket_0', 'bucket_3', 'black')
+	print '2: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_3', 'bucket_0', 'black')
+	print '3: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_1', 'hand_right', 'green')
+	print '4: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_1', 'hand_right', 'green')
+	print '5: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_1', 'hand_right', 'green')
+	print '6: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('bucket_0', 'hand_right', 'black')
+	print '7: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('hand_right', 'hand_left', 'green')
+	print '8: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+	WS_0.moveBall('hand_left', 'bucket_0', 'green')
+	print '9: '# + str(WS_0==WS_1)
+	print str(WS_0) + '\n-----------------------\n'# + str(WS_1)
+	print '========================================='
+
+def testSetWorldState():
+	state = {
+		'bucket_0': Bucket(black=1, red=2, green=3),
+		'bucket_1': Bucket(black=4, red=5, green=6),
+		'bucket_2': Bucket(black=7, red=8, green=9),
+		'bucket_3': Bucket(black=10, red=11, green=12),
+		'hand_left': Hand(),
+		'hand_right': Hand(black=1, green=1)
+	}
+
+	WS_0 = WorldState(init_state)
+	WS_0.setWorldState(state)
+	print str(WS_0)
+
+testWorldStateEqual()
+testMoveBalls()
+testSetWorldState()
